203bc74901963c5e05fc684b30639a1001380296
abiyababy8 <abiyababy16@gmail.com>
2025-01-24 12:48:26 +0530

    added some changes

---
+++ user-management/common.js
@@ -0,0 +1,25 @@
+class UserManagement {
+    dataBase = {
+        milan: {
+            username: 'milan',
+            email: 'milan@gmail.com',
+            password: 'milan123'
+        },
+        john: {
+            username: 'john',
+            email: 'john@gmail.com',
+            password: 'john123'
+        },
+        todd: {
+            username: 'todd',
+            email: 'todd@gmail.com',
+            password: 'todd123'
+        },
+    };
+    saveData() {
+        localStorage.setItem("userData", JSON.stringify(this.dataBase));
+    }
+    getData() {
+        this.dataBase = JSON.parse(localStorage.getItem("userData"));
+    }
+}
