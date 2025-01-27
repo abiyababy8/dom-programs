ae5bc9900e3a137d35c12fdd5e7211b953468d97
abiyababy8 <abiyababy16@gmail.com>
2025-01-27 12:27:18 +0530

    added some changes

--- user-management/common.js
+++ user-management/common.js
@@ -22,10 +22,29 @@ class UserManagement {
     getData() {
         this.dataBase = JSON.parse(localStorage.getItem("userData"));
     }
-    login(){
+    login() {
         this.getData()
     }
-    register(){
-        this.getData()
+    register() {
+        let uname = userName.value
+        let uemail = userEmail.value
+        let upassword = userPassword.value
+        if (uname !== '' && uemail !== '' && upassword !== '') {
+            this.getData();
+            //logic to add data to local storage
+            if (uname in this.dataBase) {
+                alert(`${uname} already exists in db!`)
+            }
+            else {
+                this.dataBase[uname] = { username: uname, email: uemail, password: upassword }
+                this.saveData();
+                alert(`${uname} successfully registered!`)
+            }
+        }
+        else {
+            alert("Please Fill The Form Completely!")
+        }
     }
 }
+const obj = new UserManagement();
+obj.saveData();
